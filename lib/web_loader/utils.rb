442a0f7bae8ceca2ae1ed4ac9941ccb6e8f97b90
src256 <src@srcw.net>
2023-10-07 13:42:40 +0900

    update toutf8

--- lib/web_loader/utils.rb
+++ lib/web_loader/utils.rb
@@ -1,34 +1,36 @@
 module WebLoader
   module Utils
     UTF_8 = 'UTF-8'
-    def toutf8(str, response_encoding)
+
+    def toutf8_charset(str, charset)
+      # charsetが指定されていない場合はnil
+      return nil if charset.to_s.length == 0
+      # 文字列のcharsetを変更する
+      str.force_encoding(charset)
+      # force_encodingが失敗した場合はnil
+      return nil unless str.valid_encoding?
+
+      result = nil
+      if charset =~ /#{UTF_8}/i
+        result = str
+      else
+        # エンコーディングがUTF8じゃない場合変換する
+        result = str.encode(UTF_8, invalid: :replace, undef: :replace)
+      end
+      result
+    end
+
+    def toutf8(str, charset)
       # 2022/04/04(月)
       # GITHUBのアポストロフィ(&#x2019 U+2019)が文字化け問題に対処するために新設。
       # 原因は直接Kconv.toutf8にresponse.bodyをわたしていたことなので(Kconvのguessが失敗していたと思われる)、
       # response.type_paramsを見てそれにforce_encodingすることで対処する。渡されているcharsetとWebページの文字コードが一致していればこれで問題はないはず。
-      org_str = str.dup
-      result = str
+      result = nil
       begin
-        if response_encoding.to_s.length > 0 # nilでないかつ長さが0以上
-          # responseで指定された文字コードであるとみなす
-          str.force_encoding(response_encoding)
-          if str.valid_encoding?
-            # 指定された文字コードとみなせた場合
-            if response_encoding != UTF_8
-              # エンコーディングがUTF8じゃない場合返還する
-              result = str.encode(UTF_8, invalid: :replace, undef: :replace)
-            else
-              # UTF8の場合そのまま
-              result = str
-            end
-          else
-            # 指定された文字コードとみなせない場合元の文字列を返す
-            result = org_str
-          end
-        else
-          # responseで文字コードが指定されていない場合Kconvを使用
-          result = Kconv.toutf8(str)
-        end
+        # 指定されたcharsetで変換する
+        result = toutf8_charset(str.dup, charset)
+        # charsetによる変換が失敗した場合Kconvを使用
+        result = Kconv.toutf8(str) if result.nil?
       rescue => ex
         puts ex.message
       end
